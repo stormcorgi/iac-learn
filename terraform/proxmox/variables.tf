a09416efaeb8574b7447bd13bcbeff2759b9dd95
stormcorgi <70886974+stormcorgi@users.noreply.github.com>
2023-06-24 23:23:52 +0900

    Add github-runner host for CD/CI with GithubActionos

--- terraform/proxmox/variables.tf
+++ terraform/proxmox/variables.tf
@@ -1,7 +1,7 @@
-variable "provision-test_ciuser" { type = string }
-variable "provision-test_cipassword" { type = string }
-variable "provision-test_ipconfig0" { type = string }
-variable "provision-test_sshkeys" { type = string }
+variable "github-runner_ciuser" { type = string }
+variable "github-runner_cipassword" { type = string }
+variable "github-runner_ipconfig0" { type = string }
+variable "github-runner_sshkeys" { type = string }
 
 variable "proxmox_api_url" { type = string }
 variable "proxmox_api_token_id" { type = string }
