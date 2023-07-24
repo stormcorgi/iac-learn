d281b5a9ad25de9f8c321317700b94f9ea351211
stormcorgi <70886974+stormcorgi@users.noreply.github.com>
2023-07-24 23:51:45 +0900

    now manage mastodon containers with swarm(stack)

--- ansible/update_mastodon.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-ansible-playbook -i inventory update_mastodon.yml
-ansible-playbook -i inventory update_mastodon.yml --tags restart -l spica
-sleep 30
-ansible-playbook -i inventory update_mastodon.yml --tags restart -l altair
-sleep 30
